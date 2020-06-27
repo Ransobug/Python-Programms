cbfcfd6e0b6373a94c1fbcfef1160b28d8308589
Your Name <ransi@localhost.localdomain>
2020-06-27 05:32:21 +0530

    Add existing file

---
+++ Finding Positive Or Negative Numbers.py
@@ -0,0 +1,7 @@
+x = float (input("Insert ypur number"))
+if x>0:
+     print("The given number is Positive")
+elif x<0:
+     print("The given number is Negative")
+else:
+     print("The given number is Zero")
